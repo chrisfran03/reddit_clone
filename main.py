346e083ce84a0ef60816cbc25ab42d6e9d11fd91
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-11-09 13:47:29 +1300

    login and signup page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello_world():
+#     return render_template("index.html")
+#
+# if __name__=='__main__':
+#     app.run(debug=True)
+from website._init_ import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
-
-if __name__=='__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
