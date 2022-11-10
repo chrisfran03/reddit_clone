522da710380609e14631dcfdbd5925703b907772
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-11-10 16:36:37 +1300

    creating and deleting posts

--- website/_init_.py
+++ website/_init_.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -6,9 +6,10 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "reddit"
+    app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -18,7 +19,7 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
+    from .models import User, Post
 
     with app.app_context():
         db.create_all()
@@ -32,3 +33,9 @@ def create_app():
         return User.query.get(int(id))
 
     return app
+
+
+# def create_database(app):
+#     if not path.exists("website/" + DB_NAME):
+#         db.create_all(app=app)
+#         print("Created database!")
