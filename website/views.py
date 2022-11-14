591626b6993dd4901f8995829ed9b93afc7e8b6a
chrisfran03 <270155795@yoobeestudent.ac.nz>
2022-11-15 11:47:29 +1300

    fixed bug in deleting posts

--- website/views.py
+++ website/views.py
@@ -43,6 +43,15 @@ def delete_post(id):
         db.session.commit()
         flash('Post deleted', category='success')
 
+    comment = Comment.query.filter(Comment.post_id == id).all()
+    if not comment:
+        pass
+    else:
+        for c in comment:
+            db.session.delete(c)
+        db.session.commit()
+
+
     return redirect(url_for('views.home'))
 
 @views.route("/posts/<username>")
@@ -81,7 +90,7 @@ def delete_comment(comment_id):
     comment = Comment.query.filter_by(id=comment_id).first()
 
     if not comment:
-        flash('Comment does not esixt', category='error')
+        flash('Comment does not exist', category='error')
     elif current_user.id != comment.author and current_user.id != comment.post.author:
         flash('You do not have permission to delete this comment', category='error')
     else:
